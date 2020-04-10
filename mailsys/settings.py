6828e14fd1c2cc59e83388aa4a63644aa0387f32
Shufan Li <jacklishufan@berkeley.edu>
2020-04-09 17:51:14 -0700

    s

--- mailsys/settings.py
+++ mailsys/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#ij!4s0#yzqz(o+@m3xyx88ucyavtns+-c&-+%mr5fzd=#d6@+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
