b75ed8f51a0f323880a4ec71753d732a32971a84
Shufan Li <jacklishufan@berkeley.edu>
2020-04-10 06:11:36 -0700

    s

--- mailsys/wsgi.py
+++ mailsys/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mailsys.settings')
 
 application = get_wsgi_application()
+sys.path.append('/usr/local/python3/lib/python3.7/site-packages')
